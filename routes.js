7556bf5dbfc925eccfea2d5ce9671e5d10cd8f5b
Tal Ben-Ari <talbenari1@gmail.com>
2016-02-13 09:23:31 -0800

    Added a couple of empty routes

--- routes.js
+++ routes.js
@@ -4,4 +4,16 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     res.render('index.html')
   })
+
+  app.get('/l/:id', (req, res) => {
+    // return the trip
+  })
+
+  app.post('/l/:id', (req, res) => {
+    // create a new trip
+  })
+
+  app.get('/search', (req, res) => {
+    // return a series of results
+  })
 }
