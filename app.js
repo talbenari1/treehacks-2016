e56376e02ba2dee61522a6ce730db6b27940c1c2
Tal Ben-Ari <talbenari1@gmail.com>
2016-02-13 08:32:45 -0800

    Fixed linting errors

--- app.js
+++ app.js
@@ -2,36 +2,36 @@
 
 // if modules need to be installed, override the default error message
 // with a more understandable one
-process.on('uncaughtException', e => {
+process.on('uncaughtException', (e) => {
   if (e.code === 'MODULE_NOT_FOUND') {
     /* eslint-disable no-console */
-    console.log('Module(s) not found. Please run \'npm install\' before trying again.');
+    console.log('Module(s) not found. Please run \'npm install\' before trying again.')
     /* eslint-enable no-console */
-    process.exit();
+    process.exit()
   }
-});
+})
 
 const express = require('express')
 const nunjucks = require('nunjucks')
 
-const config = require('./config.js');
+const config = require('./config.js')
 
 const app = express()
 
-app.set('port',  process.env.PORT || config.server.port || 3000);
+app.set('port', process.env.PORT || config.server.port || 3000)
 
 app.locals({ 'app': config.app })
 
 const logger = require('./logger.js')
-app.use(require('morgan')('dev', { 'stream': logger.stream }));
+app.use(require('morgan')('dev', { 'stream': logger.stream }))
 
 nunjucks.configure('views', {
   'autoescape': true,
   'express': app,
   'watch': config.server.development // reload templates when in development
-});
+})
 
-require('./routes.js')(app);
+require('./routes.js')(app)
 
 const server = app.listen(app.get('port'), () => {
   logger.info(`Server listening on port ${server.address().port}`)
